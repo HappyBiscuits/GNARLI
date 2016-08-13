031949b1cdf89b48735816dfb12e6ad17506902b
Alex McIvor <alexmcivor@gmail.com>
2016-08-13 01:47:42 +0100

    Added logging toggle to ui. Added logging section to config and bool ini config type

---
+++ NetworkStatusMonitor/Config/BoolConfigValue.cs
@@ -0,0 +1,22 @@
+﻿using IniParser.Model;
+
+namespace GNARLI
+{
+    public class BoolConfigValue : ConfigValue<bool>
+    {
+        public BoolConfigValue(ConfigSection section, ConfigSetting setting, bool defaultValue) : base(section, setting, defaultValue)
+        {
+        }
+
+        public override void ReadData(IniData data)
+        {
+            Validate(data);
+            var val = true;
+           
+            if (bool.TryParse(data[Section.ToString()][Setting.ToString()], out val))
+            {
+                Value = val;
+            }
+        }
+    }
+}
